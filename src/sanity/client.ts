c3b44281398f96deb36b80aa03407fd71579c67f
SANANAZ00 <snaz4598@gmail.com>
2025-07-18 23:05:42 +0500

    Add env var error handling and update Sanity API version for Studio fix

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2023-01-01',
+  apiVersion: '2024-06-19',
   useCdn: false, // `false` if you want to ensure fresh data
   token: process.env.SANITY_API_READ_TOKEN,
 }); 
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,12 +1,18 @@
-export const apiVersion = '2025-06-19';
-
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'bs73652t';
+export const apiVersion = '2024-06-19';
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
+  if (v === undefined || v === '') {
     throw new Error(errorMessage);
   }
   return v;
 }
+
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing NEXT_PUBLIC_SANITY_DATASET'
+);
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing NEXT_PUBLIC_SANITY_PROJECT_ID'
+);
