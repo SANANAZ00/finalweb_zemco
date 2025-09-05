efcb40a09f4a4e93f999681671db405f0605aad3
SANANAZ00 <snaz4598@gmail.com>
2025-09-05 17:10:23 +0500

    Remove FeaturedCategories component and fix Sanity environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from '@/components/Navbar';
 import HeroSection from '@/components/HeroSection';
-import FeaturedCategories from '@/components/FeaturedCategories';
 import ProductGrid from '@/components/ProductGrid';
 import Footer from '@/components/Footer';
 
@@ -9,7 +8,6 @@ export default function HomePage() {
     <main className="bg-pink-50 min-h-screen">
       <Navbar />
       <HeroSection id="home" />
-      <FeaturedCategories id="categories" />
       <ProductGrid />
       <Footer />
     </main>
